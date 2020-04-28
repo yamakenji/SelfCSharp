20e1770636c28e408ed8e8a947a37e94aac6cba6
KENJISBOX\yamak <k_y@goodenough.jp>
2020-04-28 10:25:55 +0900

    継承

---
+++ SelfCSharp/Ch08/InheritBasic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SelfCSharp.Ch08.Prop;
+
+namespace SelfCSharp.Ch08
+{
+    class BusinessPerson : Person
+    {
+        public string Work()
+        {
+            return $"{this.LastName}{this.FirstName}は働きます。";
+        }
+    }
+    public class InheritBasic
+    {
+        static void Main(string[] args)
+        {
+            var bp = new BusinessPerson
+            {
+                FirstName = "太郎",
+                LastName = "山田"
+            };
+            Console.WriteLine(bp.Work());
+            Console.WriteLine(bp.Show());
+        }
+    }
+}
