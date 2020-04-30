5ff8e6498bd03143453ce7776b538d3b48a837ae
KENJISBOX\yamak <k_y@goodenough.jp>
2020-04-30 18:05:47 +0900

    NestedBasic.csを追加

---
+++ SelfCSharp/Ch09/NestedBasic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SelfCSharp.Ch09
+{
+    class MyClass
+    {
+        class MyHelper // internal 同じアセンブリの中からアクセスが可能
+        {
+            public void Show()
+            {
+                Console.WriteLine("Nested Class is running!");
+            }
+        }
+
+        public void Run()
+        {
+            var helper = new MyHelper();
+            helper.Show();
+        }
+    }
+    
+    public class NestedBasic
+    {
+        static void Main(string[] args)
+        {
+            var c = new MyClass();
+            c.Run();
+            
+            // var h = new MyClass.MyHelper();
+        }    
+    }
+}
