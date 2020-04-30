3531d23b6fd977dd3619c5954fde770107abc7b7
KENJISBOX\yamak <k_y@goodenough.jp>
2020-04-30 12:45:29 +0900

    Chp9.2 練習問題

---
+++ SelfCSharp/Ch09/Practice92.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace SelfCSharp.Ch09
+{
+    enum Weekday
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+        All = 99,
+    }
+    
+    public class Practice92
+    {
+        static void Main(string[] args)
+        {
+            var w = Weekday.All;
+            Console.WriteLine(w.ToString("D"));
+        }
+        
+        
+    }
+}
