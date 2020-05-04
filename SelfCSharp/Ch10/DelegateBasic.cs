f658114d9847e30990f078a9a45289e8c538a853
KENJISBOX\yamak <k_y@goodenough.jp>
2020-05-04 16:56:35 +0900

    第10章 Delegate追加

---
+++ SelfCSharp/Ch10/DelegateBasic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SelfCSharp.Ch10
+{
+    delegate void Process(string str);
+    
+    public class DelegateBasic
+    {
+        static void Run(string s)
+        {
+            Console.WriteLine($"{s}走ります。");
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Process(Run);
+            p("ちょこちょこ");
+        }
+    }
+}
---
+++ SelfCSharp/Ch10/DelegateUse.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SelfCSharp.Ch10
+{
+    delegate void OutputProcess(string str);
+    
+    public class DelegateUse
+    {
+        void ArrayWaik(string[] data, OutputProcess output)
+        {
+            foreach (var value in data)
+            {
+                output(value);
+            }
+        }
+
+        static void AddQuote(string data)
+        {
+            Console.WriteLine($" [{data}] ");
+        }
+
+        static void AddComma(string data)
+        {
+            Console.WriteLine($" '{data}' ");
+        }
+
+        static void Front4(string data)
+        {
+            Console.WriteLine(data.Substring(0, 4));
+        }
+
+        static void Main(string[] args)
+        {
+            var data = new[] {"あかまきがみ", "あおまきがみ", "きまきがみ"};
+            var du = new DelegateUse();
+            var c = new Counter();
+            OutputProcess proc1 = AddQuote;
+            proc1 += Front4;
+            OutputProcess proc2 = AddComma;
+            du.ArrayWaik(data, proc1);
+            du.ArrayWaik(data, proc2);
+            du.ArrayWaik(data, c.AddLength);
+            Console.WriteLine(c.Result);
+        }
+    }
+
+    class Counter
+    {
+        public int Result { get; private set; }
+
+        public void AddLength(string value)
+        {
+            Result += value.Length;
+        }
+    }
+}
